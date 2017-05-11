b0709552b6cc08a6bc619c10e9649992f4bd34b2
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-05-11 19:48:35 +0900

    managers commit

---
+++ SchrodingerBit/Assets/scripts/ButtonEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+
+    public void OnStartButton()
+    {
+        CGameManager.Instance.SetCurrentState(GameState.Prepare);
+    }
+}
---
+++ SchrodingerBit/Assets/scripts/managers/CFadeManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CFadeManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    private static CFadeManager instance;
+
+    public static CFadeManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (CFadeManager)FindObjectOfType(typeof(CFadeManager));
+
+                if (instance == null)
+                {
+                    Debug.LogError(typeof(CFadeManager) + "is nothing");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    #endregion Singleton
+
+    private float fadeAlpha = 0; // フェード中の透明度
+
+    private bool isFading = false; // フェード中かどうか
+
+    public Color fadeColor = Color.black; // フェード色
+
+
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void OnGUI()
+    {
+
+        // Fade
+        if (this.isFading)
+        {
+            // 色と透明度を更新して白テクスチャを描画 .
+            this.fadeColor.a = this.fadeAlpha;
+            GUI.color = this.fadeColor;
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Texture2D.whiteTexture);
+        }
+
+
+
+    }
+
+    // 画面遷移 
+
+    // scene シーン名
+    // interval 暗転にかかる時間(秒)
+    public void LoadScene(string scene, float interval)
+    {
+        StartCoroutine(TransScene(scene, interval));
+    }
+
+
+    /// シーン遷移用コルーチン
+
+    // scene シーン名
+    // intreval 暗転にかかる時間(秒)
+    private IEnumerator TransScene(string scene, float interval)
+    {
+        // だんだん暗く .
+        this.isFading = true;
+        float time = 0;
+        while (time <= interval)
+        {
+            this.fadeAlpha = Mathf.Lerp(0f, 1f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+        }
+
+        // シーン切替 .
+        SceneManager.LoadScene(scene);
+
+
+        // だんだん明るく .
+        time = 0;
+        while (time <= interval)
+        {
+            this.fadeAlpha = Mathf.Lerp(1f, 0f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+        }
+
+        this.isFading = false;
+    }
+}
